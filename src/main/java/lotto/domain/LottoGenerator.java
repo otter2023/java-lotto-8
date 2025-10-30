726a78ab9177b013727fd5abcef607ede759e0df
Runea <otter2023@naver.com>
2025-10-31 01:28:48 +0900

    feat(domain): 로또 한 장을 생성하는 LottoGenerator 객체 생성

---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LottoGenerator {
+
+    public static Lotto generate() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+
+        return new Lotto(numbers);
+    }
+}
