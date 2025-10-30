b2ce8bd19ac1e539a883ce95a453e2e667b060f3
Runea <otter2023@naver.com>
2025-10-31 01:33:47 +0900

    feat(domain): Lotto의 Rank 계산 로직 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 
@@ -16,5 +16,15 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public Rank calculateRank(Lotto winningLotto, int bonusNumber) {
+        int matchCount = matchCount(winningLotto);
+        boolean hasBonus = containNumber(bonusNumber);
+        return Rank.of(matchCount, hasBonus);
+    }
+
+    private boolean containNumber(int bonusNumber) {
+    }
+
+    private int matchCount(Lotto winningLotto) {
+    }
 }
