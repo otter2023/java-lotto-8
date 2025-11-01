1de7e4c7c538fc5f46c982bce577104e0418fe2d
Runea <otter2023@naver.com>
2025-11-02 01:24:00 +0900

    feat(service): 총 수익 계산 로직 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -9,12 +9,21 @@ import java.util.Map;
 
 public class LottoService {
 
-    public Lottos createLottos(int amount) {
-        Money money = new Money(amount);
-        return new Lottos(money);
+    public Lottos createLottos(Money money) {
+        int count = money.getValue() / 1000;
+        return new Lottos(count);
     }
 
     public Map<Rank, Integer> calculateRanks(Lottos lottos, Lotto winningLotto, int bonusNumber) {
         return lottos.calculateRanks(winningLotto, bonusNumber);
+    }
+
+    public int calculatePrize(Map<Rank, Integer> result) {
+        int prize = 0;
+        for (Rank rank : result.keySet()) {
+            int count = result.get(rank);
+            prize += rank.getPrize() * count;
+        }
+        return prize;
     }
 }
