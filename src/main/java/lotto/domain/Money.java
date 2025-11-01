b865913fd7d9749822b2795b15a2db5c18e24133
Runea <otter2023@naver.com>
2025-11-01 22:19:32 +0900

    feat(domain): Money 객체 생성

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+public class Money {
+    private final int value;
+
+    public Money(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,6 +2,7 @@ package lotto.service;
 
 import lotto.domain.Lotto;
 import lotto.domain.Lottos;
+import lotto.domain.Money;
 import lotto.domain.Rank;
 
 import java.util.Map;
@@ -9,7 +10,8 @@ import java.util.Map;
 public class LottoService {
 
     public Lottos createLottos(int amount) {
-        return new Lottos(amount);
+        Money money = new Money(amount);
+        return new Lottos(money);
     }
 
     public Map<Rank, Integer> calculateRanks(Lottos lottos, Lotto winningLotto, int bonusNumber) {
