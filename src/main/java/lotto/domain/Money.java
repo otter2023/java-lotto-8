4c0e2a9c7bfa4a6bd7d2316a74c4554c86948410
Runea <otter2023@naver.com>
2025-11-01 22:20:20 +0900

    feat(domain): 구입 금액 단위 검증 로직 구현

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -4,7 +4,14 @@ public class Money {
     private final int value;
 
     public Money(int value) {
+        validate(value);
         this.value = value;
+    }
+
+    private void validate(int value) {
+        if (value % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000으로 나누어 떨어져야 합니다.");
+        }
     }
 
     public int getValue() {
