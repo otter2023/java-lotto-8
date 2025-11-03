9eba0215d60ce4311f45f8622a0c71db95a52a1f
Runea <otter2023@naver.com>
2025-11-03 14:19:15 +0900

    feat(controller): 에러 발생 시 에러 메시지 출력 후 해당 부분 부터 다시 입력 받는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,15 +1,12 @@
 package lotto.controller;
 
-import lotto.domain.Lotto;
-import lotto.domain.Lottos;
-import lotto.domain.Money;
-import lotto.domain.Rank;
+import lotto.domain.*;
 import lotto.service.LottoService;
 import lotto.utils.LottoParser;
 import lotto.validator.LottoValidator;
-import utils.Parser;
 import lotto.view.LottoInputView;
 import lotto.view.LottoOutputView;
+import utils.Parser;
 
 import java.util.Map;
 
@@ -26,21 +23,55 @@ public class LottoController {
     }
 
     public void run() {
-        int amount = Parser.stringToInt(inputView.amount());
-        Money money = new Money(amount);
-        Lottos purchaselottos = service.createLottos(money);
+        Money money = readAmount();
 
-        outputView.purchasedLottos(purchaselottos);
+        Lottos purchasedLottos = service.createLottos(money);
+        outputView.purchasedLottos(purchasedLottos);
 
-        Lotto winningLotto = LottoParser.stringToLotto(inputView.winningNumbers());
-        int bonusNumber = Parser.stringToInt(inputView.bonusNumber());
+        Lotto winningLotto = readWinningLotto();
 
-        LottoValidator.validateBonusNotDuplicate(winningLotto, bonusNumber);
+        int bonusNumber = readBonusNumber(winningLotto);
 
-        Map<Rank, Integer> result = service.calculateRanks(purchaselottos, winningLotto, bonusNumber);
+        Map<Rank, Integer> result = service.calculateRanks(purchasedLottos, winningLotto, bonusNumber);
         int prize = service.calculatePrize(result);
 
         outputView.ranksResult(result, prize);
         outputView.totalRateOfPrize(money, prize);
+    }
+
+    private Money readAmount() {
+        while (true) {
+            try {
+                String input = inputView.amount();
+                int value = Parser.stringToInt(input);
+                return new Money(value);
+            } catch (IllegalArgumentException | IllegalStateException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private Lotto readWinningLotto() {
+        while (true) {
+            try {
+                String input = inputView.winningNumbers();
+                return LottoParser.stringToLotto(input);
+            } catch (IllegalArgumentException | IllegalStateException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    
+    private int readBonusNumber(Lotto winningLotto) {
+        while (true) {
+            try {
+                String input = inputView.bonusNumber();
+                int bonus = Parser.stringToInt(input);
+                LottoValidator.validateBonusNotDuplicate(winningLotto, bonus);
+                return bonus;
+            } catch (IllegalArgumentException | IllegalStateException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
